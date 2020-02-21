7a3b5a2a146ec626a13e45d8b1a446549a67f827
Mohamad El-Wakeel <m.elwakeel@shiftebusiness.com>
2020-02-22 01:03:41 +0200

    shops in home page

--- src/components/popular-shops.js
+++ src/components/popular-shops.js
@@ -1,11 +1,83 @@
 import React, { Component } from 'react'
 
-export default class PopularShops extends Component {
-    render() {
-        return (
-            <div>
-                PopularShops
-            </div>
-        )
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+//import tileData from './tileData';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+  },
+  title: {
+    color: theme.palette.primary.light,
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+}));
+
+
+const tileData = [
+    {
+    img: 'https://material-ui.com/static/images/grid-list/breakfast.jpg',
+    title: 'Image',
+    author: 'author',
+    },
+    {
+    img: 'https://material-ui.com/static/images/grid-list/burgers.jpg',
+    title: 'Image',
+    author: 'author',
     }
+];
+
+export default function PopularShops() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <GridList className={classes.gridList} cols={2.5}>
+        {tileData.map(tile => (
+          <GridListTile key={tile.img}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              classes={{
+                root: classes.titleBar,
+                title: classes.title,
+              }}
+              actionIcon={
+                <IconButton aria-label={`star ${tile.title}`}>
+                  <StarBorderIcon className={classes.title} />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
 }
+
+// export default class PopularShops extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 PopularShops
+//             </div>
+//         )
+//     }
+// }
---
+++ src/components/tileData.js
@@ -0,0 +1,12 @@
+const tileData = [
+    {
+    img: 'https://material-ui.com/static/images/grid-list/breakfast.jpg',
+    title: 'Image',
+    author: 'author',
+    },
+    {
+    img: 'https://material-ui.com/static/images/grid-list/burgers.jpg',
+    title: 'Image',
+    author: 'author',
+    }
+]
